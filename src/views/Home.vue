c1ca4af90469a777ae34df4aa2198115da1f876b
pcsishun <pcsishun@Gmail.com>
2021-09-17 14:29:24 +0700

    update openup camera

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,49 @@
     <section class="topic">
       <h2 style="text-align:center">ตลาดสินค้าเฟอร์นิเจอร์และของตกแต่งบ้านออนไลน์</h2>
       <h1><b>ค้นหาง่าย</b>ทั้งไอเดียและสินค้า</h1>
-      <button> ค้นหาสินค้า </button>
+
+      <!-- Camera  -->
+      <!-- <button  id="myBtn"> ค้นหาสินค้า </button> -->
+      <div id="app-cam">
+        <div class="camera-button">
+          <button type="button" class="button is-rounded" :class="{ 'is-primary' : !isCameraOpen, 'is-danger' : isCameraOpen}" @click="toggleCamera">
+            <span v-if="!isCameraOpen">ค้นหาสินค้า</span>
+            <span v-else>Close Camera</span>
+          </button>
+        </div>
+    
+        <div v-show="isCameraOpen && isLoading" class="camera-loading">
+          <ul class="loader-circle">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+    
+        <div v-if="isCameraOpen" v-show="!isLoading" class="camera-box" :class="{ 'flash' : isShotPhoto }">
+          
+          <div class="camera-shutter" :class="{'flash' : isShotPhoto}"></div>
+            
+          <video v-show="!isPhotoTaken" ref="camera" :width="450" :height="337.5" autoplay></video>
+          
+          <canvas v-show="isPhotoTaken" id="photoTaken" ref="canvas" :width="450" :height="337.5"></canvas>
+        </div>
+    
+        <div v-if="isCameraOpen && !isLoading" class="camera-shoot">
+          <button type="button" class="button" @click="takePhoto">
+            <img src="https://img.icons8.com/material-outlined/50/000000/camera--v2.png">
+          </button>
+        </div>
+    
+        <div v-if="isPhotoTaken && isCameraOpen" class="camera-download">
+          <a id="downloadPhoto" download="my-photo.jpg" class="button" role="button" @click="downloadImage">
+            Download
+          </a>
+        </div>
+      </div>
+
+    <!-- Camera End -->
+
     </section>
 
     <section class="image">
@@ -75,27 +117,102 @@
 <script>
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+// import '../assets/js/cameraSet.js';
 
 export default {
   name: 'Home',
+  el: "#app-cam",
   components: {
     Splide,
     SplideSlide,
   },
   data:() => {
     return {
-      options: {
+      options: 
+      {
         rewind : true,
         width  : 800,
         gap    : '1rem',
+      },
+      isCameraOpen: false,
+      isPhotoTaken: false,
+      isShotPhoto: false,
+      isLoading: false,
+      link: '#'
+    }
+  },
+    
+    methods: {
+      toggleCamera() {
+        if(this.isCameraOpen) {
+          this.isCameraOpen = false;
+          this.isPhotoTaken = false;
+          this.isShotPhoto = false;
+          this.stopCameraStream();
+        } else {
+          this.isCameraOpen = true;
+          this.createCameraElement();
+        }
+      },
+      
+      createCameraElement() {
+        this.isLoading = true;
+        
+        const constraints = (window.constraints = {
+                  audio: false,
+                  video: true
+              });
+  
+  
+              navigator.mediaDevices
+                  .getUserMedia(constraints)
+                  .then(stream => {
+            this.isLoading = false;
+                      this.$refs.camera.srcObject = stream;
+                  })
+                  .catch(error => {
+            this.isLoading = false;
+                      alert("May the browser didn't support or there is some errors.");
+                  });
+      },
+      
+      stopCameraStream() {
+        let tracks = this.$refs.camera.srcObject.getTracks();
+  
+              tracks.forEach(track => {
+                  track.stop();
+              });
+      },
+      
+      takePhoto() {
+        if(!this.isPhotoTaken) {
+          this.isShotPhoto = true;
+  
+          const FLASH_TIMEOUT = 50;
+  
+          setTimeout(() => {
+            this.isShotPhoto = false;
+          }, FLASH_TIMEOUT);
+        }
+        
+        this.isPhotoTaken = !this.isPhotoTaken;
+        
+        const context = this.$refs.canvas.getContext('2d');
+        context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
+      },
+      
+      downloadImage() {
+        const download = document.getElementById("downloadPhoto");
+        const canvas = document.getElementById("photoTaken").toDataURL("image/jpeg")
+      .replace("image/jpeg", "image/octet-stream");
+        download.setAttribute("href", canvas);
       }
     }
-  }
 }
 </script>
 
 <style scoped>
-
+@import '../assets/css/cameraSet.scss';
 @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500&display=swap');
 
 .contentTop {
@@ -300,6 +417,8 @@ display: block;  /* may not need this, but I've found I do */
   left: 50%;
   -ms-transform: translate(-50%, -50%);
   transform: translate(-50%, 0);
+
+
 }
 
 </style>
